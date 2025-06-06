2d205a2b546b4794d2bed89e3b0d82bdbdf93bee
choiM1ng <furigia06@naver.com>
2025-06-07 03:28:36 +0900

    시간 측정 AOP 등록

--- src/main/java/com/example/spring/SpringConfig.java
+++ src/main/java/com/example/spring/SpringConfig.java
@@ -1,5 +1,6 @@
 package com.example.spring;
 
+import com.example.spring.aop.TimeTraceAop;
 import com.example.spring.repository.JdbcTemplateMemberRepository;
 import com.example.spring.repository.JpaMemberRepository;
 import com.example.spring.repository.MemberRepository;
@@ -37,6 +38,11 @@ public class SpringConfig {
     public MemberService memberService() {
         return new MemberService(memberRepository);
     }
+
+//    @Bean
+//    public TimeTraceAop timeTraceAop() {
+//        return new TimeTraceAop();
+//    }
 
 //    @Bean
 //    public MemberRepository memberRepository() {
